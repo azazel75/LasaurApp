d916fd86d6a6e8d8a9b81936a3698707777a4b40
Alberto Berti <alberto@metapensiero.it>
2016-06-12 02:16:05 +0200

    Set emacs local variables

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil . ((esk/project-name . "LasaurApp")
+         (esk/project-license . "GNU General Public License version 3 or later")
+         (esk/project-copyright-holder . "Stefan Hechenberger <stefan@nortd.com> and others, see AUTHORS.txt")
+         (copyright-names-regexp . "Stefan Hechenberger <stefan@nortd.com> and others, see AUTHORS\\.txt")
+         (fill-column . 78)
+         ;; force reinstall of whitespace font-lock customization
+         (eval . (whitespace-color-on)))))
